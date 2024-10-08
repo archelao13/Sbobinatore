7cbaecc9694f7fd7624a440fd11a61a0df1fa7ab
Archelao <s.garau23@studenti.unica.it>
2024-10-08 12:58:33 +0200

    Inizializzazione del progetto Sbobinatore

--- AudioToText/config.py
+++ AudioToText/config.py
@@ -1,5 +1,4 @@
 # config.py
 import os
 # Usa una variabile d'ambiente per la chiave API
-#sk-lV1xyJwvK6Sqd0zugNfeiBFgNDD_hrzScIWInRwNsfT3BlbkFJRS_dDRXhGblOEsRJkFknFhvXJqsujn4nU14n3UH3gA
 openai_api_key = os.getenv("OPENAI_API_KEY")
